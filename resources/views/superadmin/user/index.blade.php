4128878613aa5ec1a1d7861970398ad7002eebc8
Zidkielm <santoniozons@gmail.com>
2025-12-08 21:57:31 +0700

    membuat folder untuk tampilan superadmin & admin

---
+++ resources/views/superadmin/user/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Data User')
+@section('menuSuperadminUser', 'active')
+@section('content')
+    @livewire('superadmin.user.index')
+@endsection
+{{-- @livewire('superadmin.user.index') --}}
